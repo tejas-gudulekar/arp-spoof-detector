1beabf3759c5ef2acb971d31c20ab6d336ebcdfb
tejas-gudulekar <56124339+tejas-gudulekar@users.noreply.github.com>
2020-05-09 21:11:00 +0530

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("sudo apt install python3-pip")
+os.system("sudo apt install scapy")
+os.system("sudo apt install python3-tk")
+os.system("sudo pip3 install argparse")
+os.system("sudo chmod +x arp_spoof_detector.py")
